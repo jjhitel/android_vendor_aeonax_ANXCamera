4eaaad4abc5d2616d09ad24121fb8fa914aeeeed
Jeong Seong Pil <jjjhitel@gmail.com>
2021-01-04 18:34:01 +0900

    ANXCamera: Remove ANXScanner

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -6,12 +6,10 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/priv-app/ANXScanner/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXScanner/lib) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system_ext/lib,$(TARGET_COPY_OUT_SYSTEM_EXT)/lib)
 PRODUCT_PACKAGES += \
     ANXCamera \
     ANXExtraPhoto \
-    ANXScanner \
     anxres
 
 PRODUCT_PROPERTY_OVERRIDES += \
